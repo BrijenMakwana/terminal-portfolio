0edc5672971a25a0b974a5a0e82476825aade4a5
Brijen Makwana <brijenma@gmail.com>
2023-01-19 10:26:25 +0530

    command history

--- app.js
+++ app.js
@@ -12,9 +12,17 @@ import {
 
 const command = document.getElementById("command");
 const commandResults = document.getElementById("command-results");
+const commandHistory = [];
+let historyIndex = 0;
 
 command.addEventListener("keydown", (e) => {
-  if (e.which == 13) {
+  if (e.which == 38 && historyIndex > 0) {
+    historyIndex--;
+    e.target.value = commandHistory[historyIndex];
+  } else if (e.which == 40 && historyIndex < commandHistory.length - 1) {
+    historyIndex++;
+    e.target.value = commandHistory[historyIndex];
+  } else if (e.which == 13) {
     const userInput = e.target.value;
 
     const typedCommand = `<div class="input-container">
@@ -27,21 +35,27 @@ command.addEventListener("keydown", (e) => {
     switch (userInput.toLowerCase()) {
       case "help":
         commandResults.innerHTML += help;
+        addToCommandHistory(userInput);
         break;
       case "whoami":
         commandResults.innerHTML += whoami;
+        addToCommandHistory(userInput);
         break;
       case "about":
         commandResults.innerHTML += about;
+        addToCommandHistory(userInput);
         break;
       case "skills":
         commandResults.innerHTML += skills;
+        addToCommandHistory(userInput);
         break;
       case "projects":
         commandResults.innerHTML += projects;
+        addToCommandHistory(userInput);
         break;
       case "achievements":
         commandResults.innerHTML += achievements;
+        addToCommandHistory(userInput);
         break;
       case "website":
         commandResults.innerHTML += website;
@@ -51,17 +65,25 @@ command.addEventListener("keydown", (e) => {
           anchor.target = "_blank";
           anchor.click();
         }, 2000);
+        addToCommandHistory(userInput);
         break;
       case "contact":
         commandResults.innerHTML += contact;
+        addToCommandHistory(userInput);
         break;
       case "clear":
         commandResults.innerHTML = "";
+        addToCommandHistory(userInput);
         break;
       default:
         commandResults.innerHTML += `<div class="command-result"><span class="command-not-found">${e.target.value}</span>: command not found. Type 'help' to view a list of available commands.</div>`;
     }
+    historyIndex = commandHistory.length;
     e.target.value = "";
     window.scrollTo(0, document.body.scrollHeight);
   }
 });
+
+const addToCommandHistory = (userCommand) => {
+  commandHistory.push(userCommand);
+};
